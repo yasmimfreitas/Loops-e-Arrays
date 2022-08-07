c7f412b0ecf88f819a485796faf4af3f329bb925
Origin <https://github.com/yasmimfreitas/ProjetoPOO.git>
2022-08-06 22:08:15 -0300

    first commit

---
+++ src/Loops/MaiorEMedia.java
@@ -0,0 +1,29 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class MaiorEMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int soma = 0;
+
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            soma = soma + numero;
+
+            if (numero > maior) maior = numero;
+
+            count = count + 1;
+        } while(count < 5);
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Média: " + (soma/5));
+
+    }
+}
